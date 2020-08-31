dc1dd7b2e0695ef1f19dbca3bc1b3f2aed0961bf
hyeonvvoo <hyeonwoo.v24@gmail.com>
2020-08-31 18:17:39 +0900

    #0831

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models").User;
+const { User, Template } = require("../models");
 const jwt = require("jsonwebtoken");
 const { verifyToken } = require("./middlewares");
 const bcrypt = require("bcrypt");
